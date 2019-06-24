84a383f09ed453bf8cc65e3b850622e85119fa68
HappyJeannie <436030684@qq.com>
2019-06-25 00:23:09 +0800

    [添加]- 添加公共组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,14 +3,18 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
 
 import Home from '@pages/Home'
 import About from '@pages/About'
+import Nav from '@components/Nav'
+import Footer from '@components/Footer'
 
 export default class RouteConfig extends Component{
   render () {
     return (
       <Router>
         <div>
+          <Nav></Nav>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
+          <Footer></Footer>
         </div>
       </Router>
     )
